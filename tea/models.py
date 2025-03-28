575f56a890664278cb310b3bcdd85f2f39248659
9730991252 <sunilkale101@gmail.com>
2025-03-28 17:09:57 +0530

    ok

--- tea/models.py
+++ tea/models.py
@@ -2,20 +2,20 @@ from django.db import models
 from owner.models import *
 # Create your models here.
 class Tea_employee(models.Model):
-    tea_shope = models.ForeignKey(Tea_shope,on_delete=models.PROTECT,null=True, default=1)
+    tea_shope = models.ForeignKey(Tea_shope,on_delete=models.PROTECT,null=True, default='')
     name=models.CharField(max_length=100)
     mobile=models.IntegerField(unique=True)
     pin = models.IntegerField()
     status=models.IntegerField(default=1)
 
 class Tea_item(models.Model):
-    tea_shope = models.ForeignKey(Tea_shope,on_delete=models.PROTECT,null=True, default=1)
+    tea_shope = models.ForeignKey(Tea_shope,on_delete=models.PROTECT,null=True, default='')
     name = models.CharField(max_length=100)
     price = models.FloatField(null=True)
     status = models.IntegerField(default=1) 
     
 class Cart(models.Model):
-    tea_shope = models.ForeignKey(Tea_shope,on_delete=models.PROTECT,null=True, default=1)
+    tea_shope = models.ForeignKey(Tea_shope,on_delete=models.PROTECT,null=True, default='')
     employee = models.ForeignKey(Tea_employee,on_delete=models.PROTECT,null=True)
     item = models.ForeignKey(Tea_item,on_delete=models.PROTECT,null=True)
     price = models.FloatField()
@@ -23,7 +23,7 @@ class Cart(models.Model):
     qty = models.IntegerField()
 
 class OrderMaster(models.Model):
-    tea_shope = models.ForeignKey(Tea_shope,on_delete=models.PROTECT,null=True, default=1)
+    tea_shope = models.ForeignKey(Tea_shope,on_delete=models.PROTECT,null=True, default='')
     employee = models.ForeignKey(Tea_employee,on_delete=models.PROTECT,null=True)
     total_price=models.FloatField(default=0,null=True)
     ordered_date = models.DateTimeField(auto_now_add=True,null=True)
@@ -31,7 +31,7 @@ class OrderMaster(models.Model):
     
     
 class OrderDetail(models.Model):
-    tea_shope = models.ForeignKey(Tea_shope,on_delete=models.PROTECT,null=True, default=1)
+    tea_shope = models.ForeignKey(Tea_shope,on_delete=models.PROTECT,null=True, default='')
     employee = models.ForeignKey(Tea_employee,on_delete=models.PROTECT,null=True)
     item=models.ForeignKey(Tea_item,on_delete=models.PROTECT,null=True)
     item_name=models.CharField(max_length=100, null=True)
@@ -44,7 +44,7 @@ class OrderDetail(models.Model):
     
     
 class Tea_category(models.Model):
-    tea_shope = models.ForeignKey(Tea_shope,on_delete=models.PROTECT,null=True, default=1)
+    tea_shope = models.ForeignKey(Tea_shope,on_delete=models.PROTECT,null=True, default='')
     name = models.CharField(max_length=100)
     order_by = models.IntegerField(default=1)
     status = models.IntegerField(default=1)
